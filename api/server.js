acc352bf905f9760e87c44da14ba972b7499baed
Rockey Simmons <codenamerockey@gmail.com>
2019-10-14 13:26:07 -0700

    added userRouter file, and connected express router to server.js file with working http method endpoint

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const userRouter = require('../userRoutes/userRouter');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use('/api/user', userRouter);
 
 module.exports = server;
